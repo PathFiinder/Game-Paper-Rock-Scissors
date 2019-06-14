b609323d87ea3fe33b1b57cf3abf065a4dec80e2
Krystian Konecki <krystian_konecki@wp.pl>
2019-06-14 16:40:28 +0200

    Creating options

--- js/game.js
+++ js/game.js
@@ -1,1 +1,35 @@
-// Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
+const btnPlay = document.querySelector('.main__button');
+const mainDiv = document.querySelector('.main');
+const gameDiv = document.querySelector('.game');
+const paper = document.querySelector('.game__image-paper');
+const rock = document.querySelector('.game__image-rock');
+const scissors = document.querySelector('.game__image-scissors');
+
+const options = ["paper", "rock", "scissors"];
+let ifChoose = false;
+
+btnPlay.addEventListener('click', () => {
+    mainDiv.classList.add('main--active');
+    gameDiv.classList.add('game--active');
+});
+
+paper.addEventListener('click', () => {
+    if (ifChoose == false) {
+        paper.classList.add('game__image--active');
+        ifChoose = true;
+    }
+});
+
+rock.addEventListener('click', () => {
+    if (ifChoose == false) {
+        rock.classList.add('game__image--active');
+        ifChoose = true;
+    }
+});
+
+scissors.addEventListener('click', () => {
+    if (ifChoose == false) {
+        scissors.classList.add('game__image--active');
+        ifChoose = true;
+    }
+});
