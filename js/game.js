00fd8b7454e36658bf502944092dd702ffba22f0
Krystian Konecki <krystian_konecki@wp.pl>
2019-06-14 20:35:22 +0200

    Final Version

--- js/game.js
+++ js/game.js
@@ -8,10 +8,21 @@ const startGame = document.querySelector('.game__start');
 const yourChoose = document.querySelector('.actual-score__user__choose');
 const pcChoose = document.querySelector('.actual-score__pc__choose');
 const winner = document.querySelector('.actual-score__winner');
+const nrGames = document.querySelector('.game__final-score__score--number');
+const nrWins = document.querySelector('.game__final-score__score--wins');
+const nrLosses = document.querySelector('.game__final-score__score--losses');
+const nrDraws = document.querySelector('.game__final-score__score--draws');
 const options = ["paper", "rock", "scissors"];
 let ifChoose = false;
+let ifActive = false;
+let numberOfGames = 0;
+let numberOfWins = 0;
+let numberOfLosses = 0;
+let numberOfDraws = 0;
 let whichIsActive;
 let pcIndex;
+let whoWin = "";
+let draw = false;
 
 
 btnPlay.addEventListener('click', () => {
@@ -31,10 +42,15 @@ for (let i = 0; i < options.length; i++) {
 const checkActive = () => {
     if (paper.classList.contains('game__image--active')) {
         whichIsActive = 0
+        ifActive = true
     } else if (rock.classList.contains('game__image--active')) {
         whichIsActive = 1
+        ifActive = true
     } else if (scissors.classList.contains('game__image--active')) {
         whichIsActive = 2
+        ifActive = true
+    } else {
+        ifActive = false
     }
 }
 
@@ -48,18 +64,52 @@ const clean = () => {
         document.querySelector(`.game__image-${options[i]}`).classList.remove('game__image--active');
         ifChoose = false;
     }
+    whoWin = "";
 }
 
+const checkWin = (userId, pcId) => {
+    draw = false;
+    if (userId == 0 && pcId == 1) {
+        whoWin = `User won`;
+        numberOfWins++;
+    } else if (userId == 0 && pcId == 2) {
+        whoWin = `Computer won`;
+        numberOfLosses++;
+    } else if (userId == 1 && pcId == 0) {
+        whoWin = `Computer won`;
+        numberOfLosses++;
+    } else if (userId == 1 && pcId == 2) {
+        whoWin = `User won`;
+        numberOfWins++;
+    } else if (userId == 2 && pcId == 0) {
+        whoWin = `User won`;
+        numberOfWins++;
+    } else if (userId == 2 && pcId == 1) {
+        whoWin = `Computer won`;
+        numberOfLosses++;
+    } else if (userId == pcId) {
+        draw = true;
+        whoWin = "Draw"
+        numberOfDraws++;
+    }
+}
 
-
-
-    startGame.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        checkActive()
+startGame.addEventListener('click', (event) => {
+    event.preventDefault();
+    checkActive()
+    if (ifActive == true) {
         yourChoose.textContent = options[whichIsActive];
         pcIndex = pcChoice();
-        console.log(pcIndex)
         pcChoose.textContent = options[pcIndex];
+        checkWin(whichIsActive,pcIndex);
+        winner.textContent = whoWin
+        numberOfGames++;
+        nrGames.textContent = numberOfGames;
+        nrWins.textContent = numberOfWins;
+        nrLosses.textContent = numberOfLosses;
+        nrDraws.textContent = numberOfDraws;
         clean();
-    })
+    } else {
+        alert("Choose one");
+    }
+})
